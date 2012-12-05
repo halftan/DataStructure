2cb5b30620589ed244c3611162d34ba662730b3b
HalFtaN <half.a.tan@gmail.com>
2012-12-05 10:13:38 +0800

    passed 1811
    Signed-off-by: HalFtaN <half.a.tan@gmail.com>

--- EOJ/1811.cpp
+++ EOJ/1811.cpp
@@ -25,10 +25,21 @@ int main(void){
     do{
         scanf("%s", buf);
         for(int i = 0; buf[i]; ++i){
-            if(isdigit(buf[i]))
-                lev = buf[i] - '0';
-            else if(isalpha(buf[i]))
+            if(isdigit(buf[i])){
+                if(isdigit(buf[i + 1])){
+                    lev = 10 * (buf[i] - '0') + buf[i + 1] - '0';
+                    ++i;
+//                  printf("inserting %d: ", lev);
+                }
+                else{
+                    lev = buf[i] - '0';
+//                  printf("inserting %d: ", lev);
+                }
+            }
+            else if(isalpha(buf[i])){
                 ch = buf[i];
+//              printf("%c(flag)\n", ch);
+            }
         }
         list[count*2] = lev;
         list[count++*2 + 1] = ch;
@@ -36,10 +47,14 @@ int main(void){
     TNode* root = newNode(list[1]);
     for(count = 1; count < N; ++count){
         TNode* ins = root;
-        for(int t = 0; t < list[count*2] - 1; ++t)
+        for(int t = 0; t < list[count*2] - 1; ++t){
+            while(ins->nsib)
+                ins = ins->nsib;
             ins = ins->fc;
+        }
         while(ins->nsib)
             ins = ins->nsib;
+//      printf("%c is child of %c\n", list[count*2 + 1], ins->elem);
         appendChild(ins, list[count*2 + 1]);
     }
     delete list;
